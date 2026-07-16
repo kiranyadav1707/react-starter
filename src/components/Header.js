7928dd19350a3ccaa1dabd96bf6186aafe34eba0
Kiran Yadav <163638109+kiranyadav1707@users.noreply.github.com>
2026-07-16 12:30:12 +0530

    Add src/components/Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>React Starter Project</h1>
+      <p>A sample React application with multiple components</p>
+    </header>
+  );
+}
+
+export default Header;
